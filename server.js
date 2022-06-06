2e297bb059940a047f47beb5f034571f09e820eb
kellumdavis <kellumdavis@gmail.com>
2022-06-06 11:29:42 -0400

    backend setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// Import Dependencies
+import express from "express";
+import cors from "cors";
+
+
+// Create our app object
+const app = express();
+
+// set up middleware
+app.use(cors());
+
+//home route for testing our app
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+
+// turn on the server listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
