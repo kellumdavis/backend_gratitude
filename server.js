609f46be3e27ea7d6500722fdec466da94495a7e
kellumdavis <kellumdavis@gmail.com>
2022-06-07 12:35:12 -0400

    testing cors fix

--- server.js
+++ server.js
@@ -3,29 +3,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import 'dotenv/config'
-import postRoutes from './routes/posts.js'
+import "dotenv/config";
+import postRoutes from "./routes/posts.js";
 import morgan from "morgan";
-
-
 
 // Create our app object
 const app = express();
 
 // set up middleware
-app.use('/posts', postRoutes)
 
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+app.use("/posts", postRoutes);
 
 //home route for testing our app
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
 
 //declare a variable for our port number
 const { PORT = 4000, MONGODB_URL } = process.env;
@@ -36,8 +33,6 @@ mongoose.connection
   .on("open", () => console.log("Your are connected to mongoose"))
   .on("close", () => console.log("Your are disconnected from mongoose"))
   .on("error", (error) => console.log(error));
-
-
 
 // turn on the server listener
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
