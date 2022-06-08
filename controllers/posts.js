65b26e2857cc7afc95b9d8a48fdd02e26a57e159
kellumdavis <kellumdavis@gmail.com>
2022-06-08 12:26:02 -0400

    added update route for posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 
@@ -22,10 +23,21 @@ export const createPost = async (req, res) => {
 
     try {
         await newPost.save();
-
+        console.log(newPost)
         res.status(201).json(newPost);
 
     } catch (error) {
-        res.status(409).json({message: error.message })
+        res.status(409).json(error)
     }
+}
+
+export const updatePost = async (req,res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true});
+
+    res.json(updatedPost);
 }
